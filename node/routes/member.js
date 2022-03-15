b7e24f6f3102442eeb04e368c2eda5349b370079
enmyuel <enmyuel@gmail.com>
2022-03-15 07:16:05 +0000

    scaled down APIs

--- node/routes/member.js
+++ node/routes/member.js
@@ -12,18 +12,56 @@ const mysql_con = mysql.createConnection({
   database: 'member_db'
 });
 
-// 입주민 전체 데이터 불러오기
-app.get('/user/resident/info', (req, res) => {
-  const sql = 'select * from MEMBER_INFO';
+// 전체 데이터 조회
+app.get('/user/info', (req, res) => {
+  arg = 4
+  var sql = 'select * from ';
+  switch(arg){
+    // 입주민
+    case 1:
+      sql = sql + 'MEMBER_INFO';
+      break;
+    // 1회 방문자
+    case 2:
+      sql = sql + 'GUEST';
+      break;
+    // 정기 방문자
+    case 3:
+      sql = sql + 'BOOKED';
+      break;
+    // 상가
+    case 4:
+      sql = sql + 'STORE';
+      break;
+  }
   mysql_con.query(sql, function(err, rows){
     if (err) console.log(err);
     res.send(rows)
   });
 });
 
-// 입주민 데이터 추가
-app.post('/user/resident/info/add', (req, res) => {
-  const sql = 'INSERT INTO MEMBER_INFO(MEMBER_NUM, NAME, DONG, HO, PHONE_NUM, MEMBER_TYPE_NUM, REMARK) VALUES (6, "홍길동", 201, 201, "010-8888-7777", 1, "");';
+// 데이터 추가
+app.post('/user/add', (req, res) => {
+  arg = 1;
+  var sql = '';
+  switch(arg){
+    // 입주민
+    case 1:
+      sql = 'INSERT INTO MEMBER_INFO(MEMBER_NUM, NAME, DONG, HO, PHONE_NUM, MEMBER_TYPE_NUM, REMARK) VALUES (6, "홍길동", 201, 201, "010-8888-7777", 1, "");';
+      break;
+    // 1회 방문자
+    case 2:
+      sql = 'INSERT INTO GUEST(NAME, VISIT_DATE, CAR_NUM, PHONE_NUM, MEMBER_NUM, MEMBER_TYPE_NUM, REMARK) VALUES ("이민호", "2022-03-15", "334마8116", "010-5534-1997", 4, 2, "가전제품설치기사")';
+      break;
+    // 정기 방문자
+    case 3:
+      sql = 'INSERT INTO BOOKED(BOOKED_PURPOSE, VALIDITY, PHONE_NUM, NAME, COMPANY_NAME, CAR_NUM, MEMBER_TYPE_NUM, REMARK) VALUES ("쓰레기수거", "2022-12-31", "010-7777-3554", "안나연", "수거1", "123호4884",  3, "")';
+      break;
+    // 상가
+    case 4:
+      sql = 'INSERT INTO STORE(STORE_NUM, STORE_NAME, PHONE_NUM, ADDR, OWNER_NAME, JOINED_DATE, WITHDREW_DATE, ACCOUNT_NUM, REMARK) VALUES (3, "미소야", "010-7332-4819", "서울특별시 어딘가", "한송이", "2022-03-11", NULL, 2, "");';
+      break;
+  }
   mysql_con.query(sql, function(err){
     if (err) console.log(err);
     res.send("1 record inserted");
@@ -31,136 +69,54 @@ app.post('/user/resident/info/add', (req, res) => {
   });
 });
 
-// 입주민 데이터 수정
-app.post('/user/resident/info/update', (req, res) => {
-  const sql = 'UPDATE MEMBER_INFO SET NAME="김길동", DONG=111, HO=111, PHONE_NUM="010-1234-9999", MEMBER_TYPE_NUM=2, REMARK="HELLO!" WHERE MEMBER_NUM=19;';
+// 데이터 수정
+app.post('/user/update', (req, res) => {
+  arg = 1;
+  var sql = '';
+  switch(arg){
+    // 입주민
+    case 1:
+      sql = 'UPDATE MEMBER_INFO SET NAME="김길동", DONG=111, HO=111, PHONE_NUM="010-1234-9999", MEMBER_TYPE_NUM=2, REMARK="HELLO!" WHERE MEMBER_NUM=19;';
+      break;
+    // 1회 방문자
+    case 2:
+      sql = 'UPDATE GUEST SET NAME="김아무개", REMARK="이름수정되었습니다" WHERE GUEST_NUM=4;';
+      break;
+    // 정기 방문자
+    case 3:
+      sql = 'UPDATE BOOKED SET NAME="소형준", CAR_NUM="888서3456", REMARK="hello!" WHERE BOOKED_NUM=2;';
+      break;
+    // 상가
+    case 4:
+      sql = 'UPDATE STORE SET STORE_NAME="김밥천국", REMARK="수정되었습니다" WHERE STORE_NUM=3;';
+      break;
+  }
   mysql_con.query(sql, function(err){
     if (err) console.log(err);
-    res.send("1 record changed");
-    console.log("1 record changed");
+    res.send("1 record updated");
+    console.log("1 record updated");
   });
 });
 
-// 입주민 데이터 삭제
-app.post('/user/resident/info/delete', (req, res) => {
-  const sql = 'DELETE FROM MEMBER_INFO WHERE MEMBER_NUM=6;';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record deleted");
-    console.log("1 record deleted");
-  });
-});
-
-// 상점 전체 데이터 불러오기
-app.get('/user/store/info', (req, res) => {
-  const sql = 'select * from STORE';
-  mysql_con.query(sql, function(err, rows){
-    if (err) console.log(err);
-    res.send(rows)
-  });
-});
-
-// 상점 데이터 추가
-app.post('/user/store/add', (req, res) => {
-  const sql = 'INSERT INTO STORE(STORE_NUM, STORE_NAME, PHONE_NUM, ADDR, OWNER_NAME, JOINED_DATE, WITHDREW_DATE, ACCOUNT_NUM, REMARK) VALUES (3, "미소야", "010-7332-4819", "서울특별시 어딘가", "한송이", "2022-03-11", NULL, 2, "");';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record inserted");
-    console.log("1 record inserted");
-  });
-});
-
-// 상점 데이터 수정
-app.post('/user/store/update', (req, res) => {
-  const sql = 'UPDATE STORE SET STORE_NAME="김밥천국", REMARK="수정되었습니다" WHERE STORE_NUM=3;';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record changed");
-    console.log("1 record changed");
-  });
-});
-
-// 상점 데이터 삭제
-app.post('/user/store/delete', (req, res) => {
-  const sql = 'DELETE FROM STORE WHERE STORE_NUM=3;'
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record deleted");
-    console.log("1 record deleted");
-  });
-});
-
-// 1회 방문자 조회
-app.get('/user/guest/info', (req, res) => {
-  const sql = 'select * from GUEST';
-  mysql_con.query(sql, function(err, rows){
-    if (err) console.log(err);
-    res.send(rows)
-  });
-});
-
-// 1회 방문자 추가
-app.post('/user/guest/add', (req, res) => {
-  const sql = 'INSERT INTO GUEST(NAME, VISIT_DATE, CAR_NUM, PHONE_NUM, MEMBER_NUM, MEMBER_TYPE_NUM, REMARK) VALUES ("이민호", "2022-03-15", "334마8116", "010-5534-1997", 4, 2, "가전제품설치기사")';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record inserted");
-    console.log("1 record inserted");
-  });
-});
-
-// 1회 방문자 수정
-app.post('/user/guest/update', (req, res) => {
-  const sql = 'UPDATE GUEST SET NAME="김아무개", REMARK="이름수정되었습니다" WHERE GUEST_NUM=4;';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record changed");
-    console.log("1 record changed");
-  });
-});
-
-// 1회 방문자 삭제
-app.post('/user/guest/delete', (req, res) => {
-  const sql = 'DELETE FROM GUEST WHERE GUEST_NUM=3;'
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record deleted");
-    console.log("1 record deleted");
-  });
-});
-
-// 정기 방문자 조회
-app.get('/user/book/info', (req, res) => {
-  const sql = 'select * from BOOKED';
-  mysql_con.query(sql, function(err, rows){
-    if (err) console.log(err);
-    res.send(rows)
-  });
-});
-
-// 정기 방문자 추가
-app.post('/user/book/add', (req, res) => {
-  const sql = 'INSERT INTO BOOKED(BOOKED_PURPOSE, VALIDITY, PHONE_NUM, NAME, COMPANY_NAME, CAR_NUM, MEMBER_TYPE_NUM, REMARK) VALUES ("쓰레기수거", "2022-12-31", "010-7777-3554", "안나연", "수거1", "123호4884",  3, "")';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record inserted");
-    console.log("1 record inserted");
-  });
-});
-
-// 정기 방문자 수정
-app.post('/user/book/update', (req, res) => {
-  const sql = 'UPDATE BOOKED SET NAME="소형준", CAR_NUM="888서3456", REMARK="hello!" WHERE BOOKED_NUM=2;';
-  mysql_con.query(sql, function(err){
-    if (err) console.log(err);
-    res.send("1 record changed");
-    console.log("1 record changed");
-  });
-});
-
-// 정기 방문자 삭제
-app.post('/user/book/delete', (req, res) => {
-  const sql = 'DELETE FROM BOOKED WHERE BOOKED_NUM=3;'
+// 데이터 삭제
+app.post('/user/delete', (req, res) => {
+  arg = 1;
+  var sql = '';
+  switch(arg){
+    // 입주민
+    case 1:
+      sql = 'DELETE FROM MEMBER_INFO WHERE MEMBER_NUM=6;';
+      break;
+    case 2:
+      sql = 'DELETE FROM GUEST WHERE GUEST_NUM=3;';
+      break;
+    case 3:
+      sql = 'DELETE FROM BOOKED WHERE BOOKED_NUM=3;';
+      break;
+    case 4:
+      sql = 'DELETE FROM STORE WHERE STORE_NUM=3;';
+      break;
+  }
   mysql_con.query(sql, function(err){
     if (err) console.log(err);
     res.send("1 record deleted");
@@ -171,8 +127,8 @@ app.post('/user/book/delete', (req, res) => {
 // 인증확인
 app.get('/user/auth', (req, res) => {
   var url = "http://52.79.193.214:8080/wp-json/wp/v2/posts";
-  username = "root";
-  password = "member";
+  username = "hong";
+  password = "12345672";
   var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
 
   const options = {
